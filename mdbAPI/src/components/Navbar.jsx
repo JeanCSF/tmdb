539c2e991d9d3734a7d6f0d812f4d674969256c3
JeanCSF <jean.csf.17@gmail.com>
2023-06-06 12:28:35 -0300

    responsive layout added

--- mdbAPI/src/components/Navbar.jsx
+++ mdbAPI/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { FaBars } from "react-icons/fa";
 import './Navbar.css'
 
 
 const Navbar = () => {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
     const [search, setSearch] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(!search) return
+        if (!search) return
 
         navigate(`/search?q=${search}`);
         setSearch("");
@@ -19,36 +21,38 @@ const Navbar = () => {
 
     return (
         <nav id="navbar">
-            <h2>
-                <Link to="/" title="Home">
-                    <BiCameraMovie />
-                    MoviesDB
-                </Link>
-            </h2>
-            <h3>
-                <Link to="top_movies" title="Get a list of movies ordered by rating.">
-                    Top Rated
-                </Link>
-            </h3>
-            <h3>
-                <Link to="upcoming" title="Get a list of movies that are being released soon.">
-                    Upcoming
-                </Link>
-            </h3>
-            <h3>
-                <Link to="popular" title="Get a list of movies ordered by popularity.">
-                    Popular
-                </Link>
-            </h3>
-            <h3>
-                <Link to="now_playing" title="Get a list of movies that are currently in theatres.">
-                    Now Playing
-                </Link>
-            </h3>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Search Movie" onChange={(e) => setSearch(e.target.value)} value={search}/>
-                <button type="submit" role="search" title="Search"> <BiSearchAlt2 /> </button>
-            </form>
+            <Link to="/" className="brand" title="Home">
+                <BiCameraMovie />
+                MoviesDB
+            </Link>
+            <button className="hamburger" title="Toggle Navbar" role="button"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                }}>
+                <FaBars />
+            </button>
+            <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
+                <ul>
+                    <li>
+                        <Link to="top_movies" title="Get a list of movies ordered by rating.">Top Rated</Link>
+                    </li>
+                    <li>
+                        <Link to="upcoming" title="Get a list of movies that are being released soon.">Upcoming</Link>
+                    </li>
+                    <li>
+                        <Link to="popular" title="Get a list of movies ordered by popularity.">Popular</Link>
+                    </li>
+                    <li>
+                        <Link to="now_playing" title="Get a list of movies that are currently in theatres.">Now Playing</Link>
+                    </li>
+                    <li>
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" placeholder="Search Movie" onChange={(e) => setSearch(e.target.value)} value={search} />
+                            <button type="submit" role="search" title="Search" id="search"> <BiSearchAlt2 /> </button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
