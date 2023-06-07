4601a4adaf3647d0e14b2f262759dd588507ea3a
JeanCSF <jean.csf.17@gmail.com>
2023-06-07 10:26:12 -0300

    SEO and acessibility updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-socials">
-                <a href="https://wa.me/5511932094609" target="_blank" rel="external"> <FaWhatsapp /> </a>
-                <a href="https://github.com/JeanCSF" target="_blank" rel="external"> <FaGithub /> </a>
-                <a href="https://www.linkedin.com/in/jean-carlos-6149a2232/" target="_blank" rel="external"> <FaLinkedin /> </a>
+                <a href="https://wa.me/5511932094609" target="_blank" rel="external" title="Watsapp"> <FaWhatsapp /> </a>
+                <a href="https://github.com/JeanCSF" target="_blank" rel="external" title="Github"> <FaGithub /> </a>
+                <a href="https://www.linkedin.com/in/jean-carlos-6149a2232/" target="_blank" rel="external" title="Linkedin"> <FaLinkedin /> </a>
             </div>
             <p><a href="https://jeancsf.github.io/portfolio" target="_blank" rel="external">JeanCSF&copy; {fullYear}</a></p>
         </footer>
