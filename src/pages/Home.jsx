e005fe6129605f059fe71ad084ce76021827b0ad
JeanCSF <jean.csf.17@gmail.com>
2023-06-07 10:38:27 -0300

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
             </p>
             <p>
                 The main part of the app was made following this <a href="https://youtu.be/XqxUHVVO7-U" target="_blank" rel="external" title="Tutorial link">tutorial</a> 
-                tutorial from Matheus Battisti of "Hora de Codar" channel on Youtube, you can find 
+                from Matheus Battisti of "Hora de Codar" channel on Youtube, you can find 
                 the original <a href="https://github.com/matheusbattisti/movies_lib" target="_blank" rel="external" title="Link to original repository">repository here</a>.
                 And here you can find the repository for my project <a href="https://github.com/JeanCSF/tmdb-react" target="_blank" rel="external" title="Link to this app repository">Repository</a>.
             </p>
